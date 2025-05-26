309286c5a456d5f94be518210ffebc65917f0953
Albin <albin@bohlin.nu>
2025-05-26 23:27:18 +0200

    more tidying up. time to sleep.

--- Program.cs
+++ Program.cs
@@ -3,14 +3,9 @@ using System.Linq.Expressions;
 
 internal class Program
 {
-
     //Python style print function giving a human readable representation of 
     //an object if possible, otherwise its the same drab Object.tostring as Console.Writeline would produce.
-    public static void Print(Object s)
-    {
-        Printmaster.Printr.Print(s);
-    }
-
+    public static void Print(Object s){ Printmaster.Printr.Print(s); }
 
     /*
      * A Prompt is an encouragement to provide input.
@@ -21,8 +16,7 @@ internal class Program
     {
         Console.WriteLine(instruction);
         string? input;
-        //Input validation not empty (string null only if terminal process killed with ctrl + z
-        do input = Console.ReadLine(); while (string.IsNullOrWhiteSpace(input));
+        do input = Console.ReadLine(); while (string.IsNullOrWhiteSpace(input)); //Input validation not empty (string null only if terminal process killed with ctrl + c)
 
         if (expected_type == typeof(System.Byte)) // If we asked user for an number. I.e. menu choice or age. *expect.equals(typeof( === false?!!?*
         {
@@ -34,29 +28,18 @@ internal class Program
             catch (OverflowException) { return ["I dont believe you are 255+ years old my friend."]; }
             catch (FormatException) { return ["Should've specified positive integers perhaps."]; }
         }
-
         return input.Split(" ");
     }
 
-
     public static void Main(string[] args)
     {
-
-
         //Main menu
-
-        var menu = new string[] { "0: Quit", "1: Test" };
-        //bool running = true; //Not neccesary, just return on Exit input.
-
-
         Print("Welcome to Excersice 2. Options below.");
-
+        var menu = new string[] { "0: Quit", "1: Test" };
         do
         {
-            foreach (var item in menu)
-            {
-                Print(item);
-            }
+            foreach (var option in menu) Print(option);//
+
             var curious =  ""; //without this curious variable i cannot access the string that procced default 
             switch (curious = Prompt($"Please enter a number between 0 and {menu.Length - 1}.", typeof(System.Byte))[0]) // Convoluted by design.. Byte becasue neither age nor menuchoice should be negative or more than 8 bits
             {
@@ -66,18 +49,8 @@ internal class Program
 
                 default: Console.WriteLine(curious + "\nInvalid input.\n"); break;
             }
-
         }
         while (true);
-
-
     }
-
-
-
-
-
-
-
 }
 
