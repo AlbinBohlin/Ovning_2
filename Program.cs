30b386d393966b8daafe5dcb03c2b2ccbf9eb93b
Albin <albin@bohlin.nu>
2025-05-26 18:53:15 +0200

    Added expected type to Prompt

--- Program.cs
+++ Program.cs
@@ -1,11 +1,6 @@
-﻿using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using static Printmaster.Printr;
+﻿internal class Program
+{
 
-internal class Program
-    {
-    
 
 
     public static void Main(string[] args)
@@ -13,50 +8,62 @@ internal class Program
 
 
         //Main menu
-        
+
         var menu = new string[] { "0: Quit", "1: Test" };
         bool running = true;
-        
-       
+
+
         print("Welcome to Excersice 2. Options below.");
-        
+
         do
         {
             foreach (var item in menu)
             {
                 print(item);
             }
-            switch (Prompt($"Please enter a number between 0 and {menu.Length- 1}.")[0])// First string in Prompt respons if more than one word.
-            {
-
-            case "0": running = false; print("Goodbye"); break;
-            case "1": running = true; break;
-            default: Console.WriteLine("Invalid input.\n");break;
-        }
             
+            switch (Prompt($"Please enter a number between 0 and {menu.Length - 1}.", typeof(int))[0]) // Convoluted by design..
+            {
+                case "0": running = false; print("Goodbye"); break;
+
+                case "1": print("testing"); break;
+
+                default: Console.WriteLine("Invalid input.\n"); break;
+            }
+
         }
         while (running);
 
 
     }
 
-    public static string[] Prompt(string instruction)
+
+    public static string[] Prompt(string instruction, Type expected_type)
     {
         Console.WriteLine(instruction);
         string? input;
-        do input = Console.ReadLine(); 
-        while (string.IsNullOrWhiteSpace(input));//Input validation
+        do input = Console.ReadLine();
+
+        while (string.IsNullOrWhiteSpace(input));//Input validation not empty (string null only if terminal process killed with ctrl + z
+
+        if (expected_type == typeof(System.Int32)) // If we asked user for a menu choice or age?? *expect.equals is false*?!
+        {
+            try { return input.Split(" "); }
+            catch (Exception) { return new string[] { "this indicator of invalid input will never be evaluated", }; } //the switch will switch on "this", which is default.
+        }
+
         return input.Split(" ");
     }
 
     //Python style print function giving a human readable representation of 
     //an object if present, otherwise its the same drab Object.tostring as Console.Writeline would produce.
-    public static void print(Object s) {
+    public static void print(Object s)
+    {
         Printmaster.Printr.Print(s);
     }
-    
 
 
-    
+
+
 }
 
