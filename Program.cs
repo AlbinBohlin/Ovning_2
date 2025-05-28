254e841ba1c1e1b434d79e9fd126414bb7c3198e
Albin <albin@bohlin.nu>
2025-05-28 13:57:33 +0200

    Done, i think

--- Program.cs
+++ Program.cs
@@ -1,7 +1,9 @@
-﻿namespace Ovning_2;
+﻿using System.Text.RegularExpressions;
+
+namespace Ovning_2;
 internal class Program
 {
-    public enum Types { Choiche, Age, Tickets, _Byte, _String } //For input validation and switch fidelity
+    public enum Types { Choiche, Age, Tickets, _Byte, _String, Sentance } //For input validation and switch fidelity
 
 
     /* A Prompt is an encouragement to provide input.
@@ -14,9 +16,9 @@ internal class Program
         string? response;
         do response = Console.ReadLine(); while (string.IsNullOrWhiteSpace(response)); //Input validation not empty (string null only if terminal process killed with ctrl + c)
 
-        if (expected_type != Types._String) // If we asked user for an number. I.e. menu choice or age. 
+        if (expected_type != Types._String && expected_type != Types.Sentance) // If we asked user for an number. I.e. menu choice or age. 
         {
-            try
+            try// Trycatch is more clean but i want to know what exception was thrown
             {
                 return new Response(System.Byte.Parse(response.Trim().Split(" ")[0]), expected_type); // First word as an Byte if eligable
             }
@@ -24,13 +26,25 @@ internal class Program
             catch (FormatException) { Console.WriteLine("Only whole positive numbers please"); return Prompt(instruction, expected_type); }
             catch (Exception e) { Console.WriteLine($"If this message prints i missed something...\n{e.Message}"); return Prompt(instruction, expected_type); }
         }
-        else// if expected is _String return the string
-        {
+        else if (expected_type == Types._String)// if expected is _String return the string
+
             return new Response(response, expected_type);
+
+        else
+        {
+            try
+            {
+                string thirdWord = Regex.Replace(response.Trim(), @"\s+", " ").Split(" ")[2];//Google to the rescue
+                return new Response(thirdWord, expected_type);
+            }
+            catch (IndexOutOfRangeException) {Console.WriteLine("Need atleast 3 words please."); return Prompt(instruction, expected_type);}
+            catch (Exception ex) { Console.WriteLine("What could this be? " + ex.Message); return Prompt(instruction, expected_type); }           
         }
-        Console.WriteLine("Not implemented yet");
-        return new Response("dfs", Types._String);
+            
+            
+        
     }
+
     /* A Response is what a Prompt returns
     @ Data - data of arbitrary type
     @ DataType - A enum describing the type of Data.*/
@@ -44,11 +58,12 @@ internal class Program
     {
         int[] total = { 0, 0 };// Number of tickets and total cost
         //Main menu
-        var menu = new string[] { "0: Quit", "1: Purchase tickets", "2: Get repetitive" };
+        var menu = new string[] { "0: Quit", "1: Purchase tickets", "2: Get repetitive", "3: Third word selector" };
         Console.WriteLine("Welcome to Excersice 2. Options below.");
 
         do
-        {
+        {   
+
             foreach (var option in menu) Console.WriteLine(option);
             Response response = Prompt("Choose option and press enter.", Types.Choiche);
             switch ((Byte)response.Data)
@@ -65,6 +80,7 @@ internal class Program
                         if (age < 5 || age > 100)
                         {
                             Console.WriteLine("No fee: 0kr.");
+                            total[0]++;
                         }
                         else if (age < 20 || age > 64)
                         {
@@ -85,13 +101,13 @@ internal class Program
                         Console.Write((String)response.Data);
                     }
                     Console.WriteLine("\nReturning to main menu..\n");
-
-
                     break;
 
                 case 3:
-
-
+                    response = Prompt("Enter a message with atleast 3 words and press enter.", Types.Sentance);
+                    Console.WriteLine($"Third word is; {(String)response.Data}.");
+                    Console.WriteLine("\nReturning to main menu..\n");
+                    break;
 
                 default: Console.WriteLine($"\nInput must be a number between 0 and  {menu.Length - 1}"); break;
             }
